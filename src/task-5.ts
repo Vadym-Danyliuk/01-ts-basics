77c4da5ec26ba74a7cb71acfec84272b975fac5d
Vadym Danyliuk <jekone13@outlook.com>
2025-08-07 22:34:03 +0300

    230

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,5 +1,5 @@
 
-function logStatus (status: "loading"| "success" | "error")  {
+function logStatus (status: "loading"| "success" | "error") :void {
   if (status === "loading") {
     console.log("Loading...");
   } else if (status === "success") {
